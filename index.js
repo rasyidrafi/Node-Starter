1f0d13840773c42d937a4bbed8055734000b71cc
Rasyid Rafi <muhammand.rafi25@gmail.com>
2021-03-16 11:22:00 +0700

    Transfer variable from back to front

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ require("dotenv").config();
 
 // Use Public Folder as Resources
 app.use(express.static("public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+app.set("views", __dirname + "/views");
 
 // Home
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+app.get("/", async (req, res) => {
+  res.render("home.html");
+});
+
+app.get("/second", async (req, res) => {
+  let angka = Math.floor(Math.random() * Math.floor(99));
+  res.render("second.html", { angkaFront: angka });
 });
 
 const listener = app.listen(process.env.PORT || 8987, () => {
